8e68a53f4de941252497ceb6c731d17afb46b40f
Parth Maheta <parthmaheta.1012@gmail.com>
2024-06-16 17:42:15 +0530

    Todo WebApp using Html css js

---
+++ script.js
@@ -0,0 +1,94 @@
+const todoForm = document.querySelector("form");
+const todoInput = document.getElementById("todo-input");
+const todoUL = document.getElementById("todo-list");
+
+let todos = getTodos();
+updateTodoList();
+
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addTodo();
+});
+
+function addTodo() {
+  const todoText = todoInput.value.trim();
+  if (todoText.length > 0) {
+    const todoObj={
+        text: todoText,
+        completed:false
+    }
+    todos.push(todoObj);
+    updateTodoList();
+    saveTodos();
+    todoInput.value = "";
+  }
+}
+function updateTodoList() {
+  todoUL.innerHTML = "";
+  todos.forEach((todo, index) => {
+    todoItem = createTodoItem(todo, index);
+    todoUL.append(todoItem);
+  });
+}
+function createTodoItem(todo, todoIndex) {
+  const todoId = "todo-" + todoIndex;
+  const todoLI = document.createElement("li");
+  const todoText=todo.text
+  todoLI.className = "todo";
+  todoLI.innerHTML = `
+        <input type="checkbox" id="${todoId}" />
+          <label for="${todoId}" class="custom-checkbox">
+            <svg
+              fill="transparent"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#5f6368"
+            >
+              <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+            </svg>
+          </label>
+          <label for="${todoId}" class="todo-text"> ${todoText} </label>
+          <button class="delete-button">
+            <svg
+              fill="red"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#5f6368"
+            >
+              <path
+                d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"
+              />
+            </svg>
+          </button>
+    
+    `;
+    const deleteButton=todoLI.querySelector(".delete-button");
+    deleteButton.addEventListener("click",()=>{
+        deletetodos(todoIndex);
+    })
+    const checkbox = todoLI.querySelector("input");
+    checkbox.addEventListener("change",()=>{
+        todos[todoIndex].completed = checkbox.checked;
+        saveTodos();
+
+    })
+    checkbox.checked=todo.completed;
+  return todoLI;
+}
+function deletetodos(todoIndex){
+    todos=todos.filter((_,i)=>i !== todoIndex );
+    saveTodos();
+    updateTodoList();
+}
+function saveTodos(){
+    const todosJson=JSON.stringify(todos);
+    localStorage.setItem("todos",todosJson);
+}
+function getTodos(){
+    const todos1=localStorage.getItem("todos") || "[]";
+    return JSON.parse(todos1);
+}
